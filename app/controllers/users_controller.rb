1175c28f74426e2886b51bea0da2748d633979dc
EC2 Default User <ec2-user@ip-172-31-16-95.ap-northeast-1.compute.internal>
2022-12-09 11:32:31 +0000

    プロフィール編集画面の作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,27 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @books = @user.books
   end
+
+  def index
+    @users = User.all
+    @user = current_user
+    @book = Book.new
+  end
+
+  def edit
+    @user = current_user
+  end
+
+  def update
+    @user = current_user
+    @user.update(user_params)
+    redirect_to user_path(@user.id)
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :profile_image, :introduction)
+  end
+
 end
